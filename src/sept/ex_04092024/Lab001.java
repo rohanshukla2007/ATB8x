2b11a37666c005323b275465c98838a07ee2d6e2
rohan <rohan.shukla07@gmail.com>
2024-09-08 18:55:30 +0530

    second push

---
+++ src/sept/ex_04092024/Lab001.java
@@ -0,0 +1,7 @@
+package sept.ex_04092024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello How are you Rohan");
+    }
+}
---
+++ src/sept/ex_04092024/Lab002.java
@@ -0,0 +1,7 @@
+package sept.ex_04092024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("I am Fine Mr. x");
+    }
+}
