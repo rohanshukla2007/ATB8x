87f022b120fda628b30f8c21f2180274d6cbeee9
rohan <rohan.shukla07@gmail.com>
2024-09-16 09:37:33 +0530

    second push

---
+++ src/sept/ex_11092024/Lab013.java
@@ -0,0 +1,21 @@
+package sept.ex_11092024;
+
+public class Lab013 {
+    public static void main(String[] args) {
+        //boolean 123 =true; this is not possible in java
+//        /
+       /* Sys**Rules to follow when you define an Identifier:**
+
+        1. Identifier can contain Alphabets, Digits, and two special symbol i.e. Dollar ($),
+                2. Underscore (_).
+        3. First character of an identifier must be an Alphabet or Dollar ($) or Underscore (_).
+        4. Keywords or Reserved words can't be used as Identifiers.
+        tem.out.println(a);
+        */
+        boolean $=true;
+        boolean _=false;
+        boolean data_type= true;
+        boolean roha123=false;
+        boolean _123=false;
+    }
+}
