f6b1dc3d0ae7324590be1cbc2273765bf2cc1ba1
rohan <rohan.shukla07@gmail.com>
2024-09-16 09:39:35 +0530

    second push

---
+++ src/sept/ex_11092024/Lab017.java
@@ -0,0 +1,16 @@
+package sept.ex_11092024;
+
+public class Lab017 {
+    public static void main(String[] args) {
+        char a='a';
+        char b ='c';
+        char c = '\n'; // Escape char for new line
+
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
