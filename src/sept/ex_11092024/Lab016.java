da6073c6b0f7f69c761e43f848c2afbe08b915a8
rohan <rohan.shukla07@gmail.com>
2024-09-16 09:39:21 +0530

    second push

---
+++ src/sept/ex_11092024/Lab016.java
@@ -0,0 +1,9 @@
+package sept.ex_11092024;
+
+public class Lab016 {
+    public static void main(String[] args) {
+        //Want to store distnace between moon and earth
+        long a=3840000000l;// in km
+        System.out.println(a);
+    }
+}
