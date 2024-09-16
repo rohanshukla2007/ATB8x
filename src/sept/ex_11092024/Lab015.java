59457a0e881d68516ecfd0ecd1285b2f5321624c
rohan <rohan.shukla07@gmail.com>
2024-09-16 09:38:59 +0530

    second push

---
+++ src/sept/ex_11092024/Lab015.java
@@ -0,0 +1,18 @@
+package sept.ex_11092024;
+
+public class Lab015 {
+    public static void main(String[] args) {
+        // These are all primitive data type
+        byte b=100;// 8 bits 1 byte
+        short c=128;// 16 bits 2 byte
+        int d=1000;// 32 bits 4 byte
+        long phone_no=1234567890l;// 64 bits 8 byte
+        long phone_no1=1234567890L;// 64 bits 8 byte
+        char grade='A';/* ASCII Value */
+        float a=3.14f;
+        float e=3.14F;/* 4 byte 32 bits */
+        double f=119.456789;// 8 byte 64 bits
+        String s ="Rohan"; // bunch of character
+
+    }
+}
