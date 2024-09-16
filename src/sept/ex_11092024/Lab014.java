f3680d7aac1b5c9e8b337f87150ced9865501f13
rohan <rohan.shukla07@gmail.com>
2024-09-16 09:38:16 +0530

    second push

---
+++ src/sept/ex_11092024/Lab014.java
@@ -0,0 +1,13 @@
+package sept.ex_11092024;
+
+public class Lab014 {
+    public static void main(String[] args) {
+        // data tyep, boolena type, numerical type, integral type
+        //but size is diff which we store in individual
+       // byte a=128; // not posible but we can store 127
+        byte b=127; // not posible but we can store 127
+        short c=128;
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
