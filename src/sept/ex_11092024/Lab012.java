3c81c2b9eb67c65141fc99b806cc713d26eddf8d
rohan <rohan.shukla07@gmail.com>
2024-09-16 09:37:17 +0530

    second push

---
+++ src/sept/ex_11092024/Lab012.java
@@ -0,0 +1,10 @@
+package sept.ex_11092024;
+
+public class Lab012 {
+    public static void main(String[] args) {
+//Variable  - Store the value
+//data type variable_name  = variable_value
+        boolean a=true;
+        System.out.println(a);
+    }
+}
